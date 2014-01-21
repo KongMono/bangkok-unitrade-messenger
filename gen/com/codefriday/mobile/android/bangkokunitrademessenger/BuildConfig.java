0d6ac175552a7c61c5b518a882fec3887a9f0a06
trasubun chankrajang <koyngben@gmail.com>
2014-01-21 09:40:53 +0700

    first commit

---
+++ gen/com/codefriday/mobile/android/bangkokunitrademessenger/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.codefriday.mobile.android.bangkokunitrademessenger;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
